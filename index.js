791f9d0241b47576ed289d052ef636e83086af3f
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-08 20:09:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function driversWithRevenueOver(drivers, revenue) {
   });
 }
 
-driverNamesWithRevenueOver() {
-  return function driversWithRevenueOver(drivers, revenue)
+function driverNamesWithRevenueOver() {
+  return driversWithRevenueOver(drivers, revenue)
 }
